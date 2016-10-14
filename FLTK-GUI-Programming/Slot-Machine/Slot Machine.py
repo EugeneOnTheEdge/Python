13dcddc4ad90064db3b250dece90d9f260bd5773
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2016-10-13 22:04:01 -0700

    Add files via upload

--- FLTK-GUI-Programming/Slot-Machine/Slot Machine.py
+++ FLTK-GUI-Programming/Slot-Machine/Slot Machine.py
@@ -5,11 +5,17 @@ import random
 import time
 
 def mix(self):
+	betAmount = int(BetInput.value())
 	_cash = cash[0]
 	wintimes = 0
 	repetition = int(AutoRollInput.value())
 	difficultyValue = int(DifficultySlider.value())
+	cashDV = int(DifficultySlider.value())
 	
+	if betAmount <= 0:
+		return
+	if (_cash - betAmount) < 0:
+		return
 	if repetition <= 0:
 		diffZero_ErrorHandler()
 		return
@@ -40,7 +46,9 @@ def mix(self):
 						if x == 2:
 							getImg2 = random.choice(pics)
 						if x == 3:
-							getImg3 = random.choice(pics)	
+							getImg3 = random.choice(pics)
+				cashToGet = betAmount + betAmount * (0.05*(-(cashDV))+0.55)
+				
 			elif mode == 'LOSE':
 				for x in range(2,4):
 					getNumber = random.choice(chances)
@@ -59,25 +67,21 @@ def mix(self):
 							getImg2 = random.choice(pics)
 						if x == 3:
 							getImg3 = random.choice(pics)
+				cashToGet = betAmount + betAmount * ((-(cashDV))*10+10)
+				
 		else:
 			getImg2 = random.choice(pics)
 			getImg3 = random.choice(pics)
+			cashToGet = betAmount + betAmount * 3
 	
 		if getImg1 == getImg2 == getImg3:
 			print repeat,'<<< WON >>>'	
 			wintimes += 1.0
-			cashToGet = 2*(difficultyValue*10 -100 + 5.0)
-			
-			if cashToGet < 0:	
-				_cash += 2 + 2*((difficultyValue*10 -100 + 5.0)*-1)
-			else:
-				_cash += cashToGet
-			print _cash
+			_cash += cashToGet
 			
 		else:
 			print repeat
-	
-	print "Winning chance %.3f%%: %i WINS, %i REPs.\n\n\n\n\n\n" % ((wintimes/repetition)*100, wintimes, repetition)
+			_cash -= betAmount
 	
 	imgUI1.image(getImg1)
 	imgUI2.image(getImg2)
@@ -87,13 +91,17 @@ def mix(self):
 	imgUI2.redraw()
 	imgUI3.redraw()
 	
-	#The next 4 lines of codes will have to do with GLOBAL-LOCAL VARIABLE thingy..
-	
+	#The next 2 lines of codes will have to do with GLOBAL-LOCAL VARIABLE thingy..
 	cash[0] = _cash
-	cashRefresh(_cash)
+	cashRefresh(_cash)		
+	
+	if _cash < 0:
+		gameOver.show()
+		
+	print "Winning chance %.2f%%: %i WINS, %i REPs.\n\n\n\n\n\n" % ((wintimes/repetition)*100, wintimes, repetition)
 		
 def DifficultySlider_Listener(self):
-	DifficultySlider.label("                                                                                      ")
+	DifficultySlider.label("                                                                                                      ")
 	difficultyValue = int(DifficultySlider.value())
 	if difficultyValue < 0:
 		mode = 'LOSE'
@@ -103,7 +111,10 @@ def DifficultySlider_Listener(self):
 	else:
 		mode = 'NORMAL'
 	if difficultyValue != 0:
-		DifficultySlider.label( ( ("DIFFICULTY HACK: %1.2f%% chance to "+mode+"!") % (difficultyValue/(maxBound*1.0) * 100) ) )
+		if mode == 'WIN':
+			DifficultySlider.label( ( ("DIFFICULTY HACK: %.2f%% chance to "+mode+"!") % (difficultyValue/(maxBound*1.0) * 100) ) )
+		else:
+			DifficultySlider.label( ( ("DIFFICULTY HACK: %.2f%% more chance to "+mode+"!") % (difficultyValue/(maxBound*1.0) * 100) ) )
 	else:
 		DifficultySlider.label("Difficulty Hack: OFF")
         
@@ -122,7 +133,8 @@ def cashRefresh(money):
 	
 def close_diffZero(self):	
 	aboveZero_win.hide()
-
+def close_betZero(self):
+	betZero.hide()
 mode ='NORMAL'
 difficultyValue = 0  
 cash = [20.0]
@@ -153,6 +165,10 @@ AutoRollTextBox = Fl_Box(200,425,300,20,"Auto-Roll:")
 AutoRollInput = Fl_Input(200,450,300,60,None)
 AutoRollInput.value(str(1))
 
+BetTextBox = Fl_Box(540,425,300,20,"Bet ($):")
+BetInput = Fl_Input(540,450,300,60,None)
+BetInput.value(str(2))
+
 maxBound = 10
 minBound = -(maxBound)
 DifficultySlider = Fl_Slider(200,600,640,60, "Difficulty Hack: OFF")
@@ -178,13 +194,24 @@ window.end()
 
 aboveZero_win = Fl_Window(500,320,400,200,"Error")
 aboveZero_win.begin()
-
-closeBtn = Fl_Return_Button(225,100,100,30,"Alright!")
+closeBtn = Fl_Return_Button(250,135,100,30,"Alright!")
 closeBtn.callback(close_diffZero)
 errorText = Fl_Box(25,0,350,150,"Mate, you gotta enter the Auto-Roll value \nBIGGER THAN 0. I hope you haven't forgotten \nbout real #s..")
-aboveZero_win.callback(diffZero_ErrorHandler)
 aboveZero_win.end()
 
+betZero = Fl_Window(500,320,400,200,"Error")
+betZero.begin()
+errorText = Fl_Box(25,0,350,150,"Yo, how can you play without betting?!\n Check ur bet amount and \nuse ur logic, mate!")
+closeBtn = Fl_Return_Button(250,135,100,30,"I will!")
+betZero.end()
+betZero.show()
+
+noMoney = Fl_Window(500,320,400,200,"GAME OVER")
+noMoney.begin()
+errorText = Fl_Box(25,0,350,150,"Nope. Go home, lazy. \nYou wasted all ur money already. \nYes, go outside and get a job.")
+closeBtn = Fl_Return_Button(100,145,200,30,"Oh shoot I'm poor now")
+noMoney.end()
+noMoney.show()
 print "/!\DO NOT CLOSE THIS (TERMINAL) WINDOW AS IT WILL CLOSE THE MAIN PROGRAM AS WELL. /!\\\n\n"
 
 window.show()
