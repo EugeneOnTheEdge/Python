bb3d9ff4aa41aff1eee4fe236d11fe5d9f488686
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2016-10-19 22:01:45 -0700

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -10,39 +10,59 @@ def loop():
 def startTime(self):
 	if not started[0]:
 		watch[0]=_time.time()
-		started[0] = True
-		
-	second = time[1]%60.0
+		started[0] = True	
+	second = time[1]
 	minute = int(second/60)
-	
-	if minute < 10:
+	modSecond = second%60
+	if minute < 1:
+		if modSecond < 10:
+			timeShow.label("0%.2f" % (modSecond))
+		else:
+			timeShow.label("%.2f" % (modSecond))
+	else:
+		if modSecond < 10:
+			if minute < 10:
+				timeShow.label("0%i:0%.2f" % (minute,modSecond))
+			else: 
+				timeShow.label("%i:0%.2f" % (minute,modSecond))
+		else:
+			if minute < 10:
+				timeShow.label("0%i:%.2f" % (minute,modSecond))
+			else: 
+				timeShow.label("%i:%.2f" % (minute,modSecond))
+	second += 0.03
+	time[0] = minute
+	time[1] = second
+	startBtn.shortcut(None)
+	stopBtn.shortcut(FL_Enter)
+	secondAnalog.value(second%60)
+	secondAnalog.color(FL_BLACK)
+	Fl.repeat_timeout(0.03,loop)
+
+def stopTime(self):
+	Fl.remove_timeout(loop)
+	stopBtn.shortcut(None)
+	startBtn.shortcut(FL_Enter)
+	started[0] = False
+	second = time[1]
+	minute = int(second/60)
+	second = second%60
+	if minute < 1:
 		if second < 10:
 			timeShow.label("0%.2f" % (second))
 		else:
 			timeShow.label("%.2f" % (second))
 	else:
 		if second < 10:
-			timeShow.label("%i:0%.2f" % (minute,second))
+			if minute < 10:
+				timeShow.label("0%i:0%.2f" % (minute,second))
+			else: 
+				timeShow.label("%i:0%.2f" % (minute,second))
 		else:
-			timeShow.label("%i:%.2f" % (minute,second))
-		
-	second += 0.02
-	time[0] = minute
-	time[1] = second
-	startBtn.shortcut(None)
-	stopBtn.shortcut(FL_Enter)
-	secondAnalog.value(second%60)
-	secondAnalog.color(FL_GREEN)
-	Fl.repeat_timeout(0.02,loop)
-	
-def stopTime(self):
-	Fl.remove_timeout(loop)
-	stopBtn.shortcut(None)
-	startBtn.shortcut(FL_Enter)
-	started[0] = False
-	minute = time[0]
-	second = time[1]
-	timeShow.label("%i:%.2f" % (minute,second))
+			if minute < 10:
+				timeShow.label("0%i:%.2f" % (minute,second))
+			else: 
+				timeShow.label("%i:%.2f" % (minute,second))
 	secondAnalog.value(0)
 	secondAnalog.color(self.labelcolor())
 
@@ -64,10 +84,11 @@ w.begin()
 w.color(fl_rgb_color(5,5,5))
 
 secondAnalog = Fl_Dial(85,70,420,420)
+secondAnalog.color2(FL_GREEN)
 secondAnalog.type(FL_FILL_DIAL)
 secondAnalog.angles(0,360)
 secondAnalog.bounds(0,60)
-secondAnalog.color(FL_GREEN)
+secondAnalog.color(FL_BLACK)
 
 bg_secAnalog = Fl_Dial(95,80,400,400)
 bg_secAnalog.color(FL_BLACK)
