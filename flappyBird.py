30b35e37f6cede73c7134506c72f0daa009908cf
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2016-11-02 21:41:45 -0700

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -2,6 +2,31 @@
 
 from fltk import *
 
+def gravity():
+	global velocity
+	global bird
+	global getY
+	global window
+	
+	velocity = velocity - (acceleration*refreshRate)
+	displacement = velocity * refreshRate
+	getY += int(displacement)
+	window.begin()
+	bird = Fl_Box(370,getY,60,50)
+	bird.image(_bird)
+	bird.redraw()
+	window.end()
+	Fl.repeat_timeout(refreshRate,gravity)
+	
+def fly(self):
+	global gameStarted
+	global velocity
+	if not gameStarted:
+		gameStarted = True
+		Fl.add_timeout(refreshRate,gravity)
+	else:
+		velocity = 20
+		 
 #Window setup
 getWidth = Fl.w()
 getHeight = Fl.h()
@@ -9,11 +34,30 @@ windowW = 800
 windowH = 600
 
 #Game setup
-acceleration = 9.8 #m/(s^2)
+_bird = Fl_PNG_Image('flappybird.png')
+_bird = _bird.copy(60,50)
+acceleration = 9.8 # m/(s^2)
+velocity = 0 # m/s
+FPS = 30
+refreshRate = 1.0/FPS
+gameStarted = False
 
-window = Fl_Window( ((getWidth-windowW)/2), ((getHeight-windowH)/2), windowW, windowH, "Flappy Burd")
+#GameWindow Begin
+window = Fl_Double_Window( ((getWidth-windowW)/2), ((getHeight-windowH)/2), windowW, windowH, "Flappy Burd")
 window.begin()
 
-window.end()
+btn = Fl_Button(0,0,5,5)
+btn.box(FL_NO_BOX)
+btn.callback(fly)
+btn.shortcut(FL_ENTER)
 
+bird = Fl_Box(370,275,60,50)
+bird.image(_bird)
+bird.redraw()
+getY = bird.y()
+
+window.end()
+#GameWindow End
+
+window.show()
 Fl.run()
