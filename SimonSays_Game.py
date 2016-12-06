827dad39bd7fc33d9f828043d6497b9a5bdcb76d
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2016-12-05 16:07:48 -0800

    Update SimonSays_Game.py

--- SimonSays_Game.py
+++ SimonSays_Game.py
@@ -7,6 +7,7 @@ import cPickle as IO
 def btn_OnClick(self):
 	global level, click, simonsaysBtns, userResponse, SimonIsSaying
 	gameover = False
+	redraw(None)
 	print buttons.index(self),
 	
 	if not SimonIsSaying:
@@ -17,23 +18,27 @@ def btn_OnClick(self):
 			userResponse.append(self)
 			click += 1
 			
-	if len(userResponse) == len(simonsaysBtns):
+	if (not SimonIsSaying) and (len(userResponse) == len(simonsaysBtns)):
 		level += 1
 		click = 0
 		startBtn.activate()
 		userResponse = []
-		SimonIsSaying = False
+		SimonIsSaying = True
 		print '\n====================='
+		startBtn.color(FL_RED)
+		statusBar.label('Correct!')
 		
 	if gameover:
 		print '======GAME OVER======'
+		statusBar.label('Game over! Nice try.')
 		level = 1
 		startBtn.activate()
 		click = 0
 		userResponse = []
 		simonsaysBtns = []
-		SimonIsSaying = False
-
+		SimonIsSaying = True
+		startBtn.color(FL_RED)
+	
 def simonsays():
 	global pressed, originalColour, SimonIsSaying, order, level
 
@@ -48,16 +53,22 @@ def simonsays():
 		pressed = False
 		order += 1
 	
+	redraw(None)
+	
 	if order < level:
 		Fl.add_timeout(0.5, simonsays)
 	else:
 		SimonIsSaying = False
 		order = 0
+		statusBar.label('Waiting for your response...')
 	
 def start_OnClick(self):
 	global _simon
 	self.deactivate()
+	statusBar.label('')
+	self.color(FL_GRAY)
 	SimonIsSaying = True
+	levelBox.label(str(level))
 	btn = random.choice(buttons)
 	simonsaysBtns.append(btn)
 	if level == 1:
@@ -65,7 +76,12 @@ def start_OnClick(self):
 	_simon.append(buttons.index(btn))
 	print ("\n\n\n====== LEVEL %i ======\nSimon says:" % level), _simon
 	print "\nYou've pressed:"
-	Fl.add_timeout(0.5, simonsays)
+	redraw(None)
+	Fl.add_timeout(1.5, simonsays)
+
+def redraw(self):
+	for wid in makeitreal:
+		wid.redraw()
 	
 #Window setup
 getWidth = Fl.w()
@@ -80,40 +96,84 @@ order = 0
 simonsaysBtns = []
 _simon = []
 userResponse = []
-SimonIsSaying = False
-pressed = False
+SimonIsSaying = True
 originalColour = None
+pressed = False
 
 #Window begin
-window = Fl_Double_Window( ((getWidth-windowW)/2), ((getHeight-windowH)/2), windowW, windowH, "Simon Says")
+window = Fl_Double_Window( ((getWidth-windowW)/2), ((getHeight-windowH)/2), windowW, windowH, "Simon Says Simulator")
 window.begin()
 Fl.scheme('gtk+')
 window.color(FL_BLACK)
 
-greenBtn = Fl_Button(100,100,200,200)
+greenBtn = Fl_Button(100,100,195,195)
 greenBtn.color(FL_GREEN)
 greenBtn.callback(btn_OnClick)
 
-redBtn = Fl_Button(300,100,200,200)
+redBtn = Fl_Button(305,100,195,195)
 redBtn.color(FL_RED)
 redBtn.callback(btn_OnClick)
 
-blueBtn = Fl_Button(100,300,200,200)
+blueBtn = Fl_Button(100,305,195,195)
 blueBtn.color(FL_BLUE)
 blueBtn.callback(btn_OnClick)
 
-yellowBtn = Fl_Button(300,300,200,200)
+yellowBtn = Fl_Button(305,305,195,195)
 yellowBtn.color(FL_YELLOW)
 yellowBtn.callback(btn_OnClick)
 
 buttons = [greenBtn,redBtn,blueBtn,yellowBtn]
 
-startBtn = Fl_Button(510,510,75,75,"Start")
+statusBar = Fl_Box(100,-150,400,400,'Press start button.')
+statusBar.labelcolor(FL_WHITE)
+statusBar.labelsize(20)
+
+#Make-it-real moment comes next...
+squareBox = Fl_Box(165,165,270,270)
+squareBox.box(FL_BORDER_BOX)
+squareBox.color(FL_BLACK)
+squareBox.color2(FL_BLACK)
+
+squareBox2 = Fl_Box(175,175,250,250)
+squareBox2.box(FL_BORDER_BOX)
+squareBox2.color(FL_GRAY)
+squareBox2.color2(FL_GRAY)
+
+squareBox3 = Fl_Box(180,180,240,106)
+squareBox3.box(FL_BORDER_BOX)
+squareBox3.color(FL_BLACK)
+squareBox3.color2(FL_BLACK)
+
+simonText = Fl_Box(180,185,240,106, "simon")
+simonText.labelcolor(FL_GRAY)
+simonText.labelsize(65)
+
+startBtn = Fl_Button(287,320,25,25)
 startBtn.box(FL_ROUND_UP_BOX)
-startBtn.color(FL_GREEN)
+startBtn.color(FL_RED)
 startBtn.callback(start_OnClick)
+
+startText = Fl_Box(280,295,40,25,"START")
+startText.labelsize(14)
+
+levelText = Fl_Box(200,325,60,40,'LEVEL')
+bestText = Fl_Box(340,325,60,40,'BEST')
+
+bestBox = Fl_Box(340,360,60,40, 'N/A')
+bestBox.color(FL_BLACK)
+bestBox.labelcolor(FL_WHITE)
+bestBox.box(FL_BORDER_BOX)
+
+levelBox = Fl_Box(200,360,60,40, str(level))
+levelBox.color(FL_BLACK)
+levelBox.labelcolor(FL_GREEN)
+levelBox.box(FL_BORDER_BOX)
+
+makeitreal = [squareBox,squareBox2,squareBox3,simonText,startBtn,startText,levelText,bestText,bestBox,levelBox]
+
+#End of widget assignments
+
 window.end()
-#Window end
 
 window.show()
 Fl.run()
