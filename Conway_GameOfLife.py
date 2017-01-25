8cf200d1eefe07dcb5399b5225dccaa8e916652b
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2017-01-24 18:44:02 -0800

    Add files via upload

--- Conway_GameOfLife.py
+++ Conway_GameOfLife.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from fltk import *
+
 #CONWAY's GAME OF LIFE RULES:
 '''
 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
@@ -10,29 +11,44 @@ from fltk import *
 '''
 
 class Pixel(Fl_Button):
-    count = 0
-    def __init__(self, x, y, w, h):
-        Fl_Button.__init__(self, x, y, w, h)
-        self.x = x
-        self.y = y
-        self.alive = False
-        self.callback(self.onClick)
-        self.alive_around = 0
-        self.box(FL_FLAT_BOX)
+	count = 0
+	def __init__(self, x, y, w, h):
+		Fl_Button.__init__(self, x, y, w, h)
+		self.x = x
+		self.y = y
+		self.alive = False
+		self.callback(self.onClick)
+		self.alive_around = 0
+		self.box(FL_THIN_DOWN_BOX)
     
-    def onClick(self,widget):
-        if self.alive:
-            self.alive = False
-            self.color(FL_GRAY)
-        else:
-            self.alive = True
-            self.color(FL_BLUE)
-        self.redraw()
-        print self, "Alive:", self.alive
-    def __repr__(self):
-        return 'Button at '+str((self.x-100)/10)+', '+str(self.y/10)
+	def onClick(self,widget):
+		global currentAlive
+		if self.alive:
+			self.alive = False
+			self.color(FL_GRAY)
+			currentAlive -= 1
+		else:
+			self.alive = True
+			self.color(FL_BLUE)
+			currentAlive += 1
+		self.redraw()
+		currentAliveBox.label('Alive: '+str(currentAlive)+' /'+str(pixelCount)+' ( %.3f%% )' % (currentAlive/pixelCount*100) )
+		if currentAlive > 0:
+			simulateBtn.activate()
+			resetBtn.activate()
+		else:
+			simulateBtn.deactivate()
+			resetBtn.deactivate()
+	def __repr__(self):
+		return 'Button at '+str((self.x)/pixelSize)+', '+str(self.y/pixelSize)
 
-def simulate(widget):
+def simulate_listener(widget):
+	global currentAlive
+	currentAlive = 0
+	stopBtn.activate()
+	widget.deactivate()
+	resetBtn.deactivate()
+	
 	for y in range(len(buttons_2d)):
 		for x in buttons_2d[y]:
 			x.alive_around = 0
@@ -64,13 +80,17 @@ def simulate(widget):
 				else:
 					x.next_alive = False
 			else:
+				currentAlive += 1
 				if x.alive_around < 2 or x.alive_around > 3:
 					x.next_alive = False
 				else:
 					x.next_alive = True
 	refresh()
-			
-def refresh():
+		
+def refresh(reset=0):
+	global generations, currentAlive
+	generations += 1
+	resetBtn.deactivate()
 	for y in range(len(buttons_2d)):
 		for x in buttons_2d[y]:
 			if x.next_alive:
@@ -79,23 +99,70 @@ def refresh():
 				x.color(FL_GRAY)
 			x.alive = x.next_alive
 			x.redraw()
-			
-window = Fl_Window(100,100,800,600,'Conway\'s Game of Life')
+	generationsBox.label('Generations: '+str(generations-reset))
+	currentAliveBox.label('Alive: '+str(currentAlive)+' /'+str(pixelCount)+' ( %.3f%% )' % (currentAlive/pixelCount*100) )
+	if currentAlive == 0 and reset != 1:
+		stopBtn.do_callback()
+	else:
+		Fl.add_timeout((1.0/FPS),simulate_listener,simulateBtn)
+	
+def stop_listener(widget):
+	global generations, currentAlive
+	generations = 0
+	widget.deactivate()
+	resetBtn.activate()
+	Fl.remove_timeout(simulate_listener)
+	if currentAlive > 0:
+		simulateBtn.activate()
+		resetBtn.activate()
+	else:
+		simulateBtn.deactivate()
+		resetBtn.deactivate()
+
+def reset_listener(widget):
+	global currentAlive,generations
+	currentAlive = 0
+	generations = 0
+	for y in range(len(buttons_2d)):
+		for x in buttons_2d[y]:
+			x.alive = 0
+			x.next_alive = 0
+	refresh(1)
+window = Fl_Double_Window(100,100,1080,600,'Conway\'s Game of Life')
 window.begin()
 
-simulateBtn = Fl_Return_Button(13,100,75,30,'Simulate')
-simulateBtn.callback(simulate)
-buttons_2d = []
-pixelSize = 10
-FPS = 30
+simulateBtn = Fl_Button(25,20,110,35,'SIMULATE')
+simulateBtn.callback(simulate_listener)
+simulateBtn.box(FL_ROUND_UP_BOX)
+simulateBtn.deactivate()
 
-for y in range(0,600,pixelSize):
+stopBtn = Fl_Button(160,20,110,35,'STOP')
+stopBtn.deactivate()
+stopBtn.callback(stop_listener)
+stopBtn.box(FL_ROUND_UP_BOX)
+
+resetBtn = Fl_Button(700,20,110,35,'RESET')
+resetBtn.deactivate()
+resetBtn.callback(reset_listener)
+resetBtn.box(FL_ROUND_UP_BOX)
+
+generationsBox = Fl_Box(300,20,120,35, 'Generations: 0')
+
+buttons_2d = []
+pixelSize = 12
+FPS = 15
+generations = 0
+currentAlive = 0
+
+for y in range(80,600,pixelSize):
     dimension = []
-    for x in range(100,800,pixelSize):
+    for x in range(0,1080,pixelSize):
         dimension.append(Pixel(x,y,pixelSize,pixelSize))
     buttons_2d.append(dimension)
+
+pixelCount = len(buttons_2d)*len(buttons_2d[0])*1.0
+currentAliveBox = Fl_Box(470,20,200,35,('Alive: '+str(currentAlive)+' /'+str(pixelCount)+' ( %.3f%% )' % (currentAlive/pixelCount*100) ))
 window.end()
 
-print buttons_2d
 window.show()
 Fl.run()
